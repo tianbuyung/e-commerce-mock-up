f62309c168497c111564e703e68acb782c946afa
Rheinma <aderhendra04@gmail.com>
2023-04-06 02:24:39 +0700

    Ade Rhendra v.1

--- middleware/isAdminMiddleware.js
+++ middleware/isAdminMiddleware.js
@@ -1,8 +1,9 @@
 module.exports = (req, res, next) => {
     if (req.session.userId && req.session.role !== "admin") {
-        const error = "You have no accsess";
+        const error = "You have no access";
         res.redirect(`/login?error=${error}`);
     } else {
         next();
     }
 }
+
