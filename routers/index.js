6da054ab5a3930e6311fc85a84e17a5610a7c27c
Septian Maulana <septianm028@gmail.com>
2023-04-06 09:51:03 +0700

    fix: fixed bugs

--- routers/index.js
+++ routers/index.js
@@ -6,25 +6,16 @@ const adminRouter = require('./admin')
 const profileRouter = require('./profiles')
 const productRouter = require('./products')
 const categoryRouter = require('./categories')
-const isLoginMiddleWare = require('../middleware/loginMiddleware')
+const isLoginMiddleWare = require('../middleware/loginMiddleware');
+const isAdminMiddleware = require('../middleware/isAdminMiddleware');
 
-router.get("/register", HomeController.registerForm);
-router.post("/register", HomeController.submitRegister);
-router.get("/login", HomeController.loginForm);
-router.post("/login", HomeController.submitLoginUser);
-router.get("/", HomeController.homePage);
-router.use("/products", productRouter);
-router.use(isLoginMiddleWare);
-router.use("/profile", profileRouter);
-router.get("/logout", HomeController.getlogout);
-module.exports = router;
-
-
-// router.get('/', Controller.homePage);
-// router.use('/', authRouter);
-// router.use('/admin', adminRouter);
-// router.use('/profiles', profileRouter);
-// router.use('/products', productRouter);
-// router.use('/categories', categoryRouter);
+router.get('/', HomeController.homePage); // done
+router.use('/', authRouter); // done
+router.use('/products', isLoginMiddleWare, productRouter);
+router.use('/profile', isLoginMiddleWare, profileRouter);
+router.use('/admin', isAdminMiddleware, adminRouter); // done
+router.use('/categories', categoryRouter);
+// router.use(isLoginMiddleWare);
+// router.use(isAdminMiddleware);
 
 module.exports = router;
--- routers/products.js
+++ routers/products.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const Controllers = require('../controllers/productController');
-const isLoginMiddleWare = require("../middleware/loginMiddleware");
-const isAdminMiddleware = require("../middleware/isAdminMiddleware");
 
 router.get('/', );
 router.get('/:id', );
-router.use(isLoginMiddleWare);
-router.use(isAdminMiddleware);
 
 module.exports = router;
